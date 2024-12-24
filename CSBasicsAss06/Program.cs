b38ffa24ba89ced9788229d09707217edf9b9903
Rania Mahmoud <Rania.Mahm555@gmail.com>
2024-12-24 23:38:46 +0200

    Add project files.

---
+++ CSBasicsAss06/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSBasicsAss06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
